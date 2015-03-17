43901d38ad9f3e1cf7a315cf00e8d8a71830747f
Coda Hale <coda.hale@gmail.com>
2015-03-17 04:00:29 +0000

    rebuild pages at f496270

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['jumphash'] = {"items":[[0,"","jumphash","An implementation of the [Jump Consistent Hash](http://arxiv.org/abs/1406.2294) by Lamping &\nVeach.",null,null],[5,"hash","","",null,{"inputs":[{"name":"t"},{"name":"u32"}],"output":{"name":"u32"}}]],"paths":[]};
+initSearch(searchIndex);
